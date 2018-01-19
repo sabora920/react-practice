ecb98d20e247acab1135e46d69db34c436b0592c
Ryan Sabo <sabora920@gmail.com>
2018-01-19 15:48:12 -0500

    finished adding state to the board component, squares add X when clicked on

---
+++ components/square.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './square.css'
+
+export class Square extends React.Component {
+
+    render() {
+      return (
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
+        </button>
+      );
+    }
+
+  }
